73b4b545969daaa1677c69935d31393187c2e9f7
tunglt.netsgame <tunglt@netsgame.vn>
2022-10-14 16:05:49 +0700

    Updated singleton accessing.

--- Assets/IAPImplementation/Scripts/IAPManager.cs
+++ Assets/IAPImplementation/Scripts/IAPManager.cs
@@ -5,9 +5,30 @@ namespace Assets.IAPImplementation.Scripts
 {
     public class IAPManager : MonoBehaviour
     {
-        public UnityAction<bool, string> OnInitialized;
-        public UnityAction<bool, string> OnPurchased;
-        public UnityAction<bool, string> OnRestored;
+        #region Singleton
+
+        private static IAPManager _instance;
+
+        public static IAPManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<IAPManager>() ??
+                        (Instantiate(Resources.Load(nameof(IAPManager))) as GameObject)
+                        .GetComponent<IAPManager>();
+                }
+
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        public UnityAction<bool, string> OnInitialized { get; set; }
+        public UnityAction<bool, string> OnPurchased { get; set; }
+        public UnityAction<bool, string> OnRestored { get; set; }
 
         [SerializeField] private IAPInitialization _initialization;
         [SerializeField] private IAPPurchasing _purchase;
@@ -15,6 +36,7 @@ namespace Assets.IAPImplementation.Scripts
 
         private void Awake()
         {
+            ConfigSingleton();
             InitializeService();
             InitializeButtonBehaviour();
             InitializeRestoringBehaviour();
@@ -42,6 +64,18 @@ namespace Assets.IAPImplementation.Scripts
                     Debug.Log($"Restoring status: {status} | {message}");
                 _restoring.SetRequiredComponents(_initialization.ExtensionProvider);
             }
+
+            void ConfigSingleton()
+            {
+                _instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
         }
+
+        public void PurchaseProduct(string productId) => 
+            _purchase.BuyProduct(productId);
+
+        public void RestoreProducts() =>
+            _restoring.RestorePurchases();
     }
 }
